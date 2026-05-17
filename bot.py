bf3ffed89f078faf60fc2d4d8798916734dc9e11
divinepranesh-ship-it <divinepranesh@gmail.com>
2026-05-17 16:12:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,8 +24,8 @@ from pytgcalls.types import (
 )
 from pytgcalls.exceptions import NoActiveGroupCall
 
-import spotify
-from spotify.oauth2 import SpotifyClientCredentials
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 from config import Config
 from database import db
